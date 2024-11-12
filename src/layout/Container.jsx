f84281e032d2f6f257ca1a8d8379681ecd5c4846
lucasjcfreire <lucasjcfreire@outlook.com>
2024-11-12 18:44:44 -0300

    style: Atualizado estilos do Footer e do Container para melhorar posicionamento da pokebola

--- src/layout/Container.jsx
+++ src/layout/Container.jsx
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 
 export default function Container(props) {
-	return <StyledMain>{props.children}</StyledMain>;
+  return <StyledMain>{props.children}</StyledMain>;
 }
 
 const StyledMain = styled.main`
   width: 100%;
   padding: 120px 20px 20px 20px;
-  height: calc(100vh - 80px);
+  height: calc(100vh - 100px);
   overflow-y: auto;
 `;
--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -2,28 +2,28 @@ import { FaAddressCard, FaGithub, FaLinkedin } from "react-icons/fa";
 import styled from "styled-components";
 
 export default function Footer() {
-	return (
-		<StyledFooter>
-			<ul>
-				<li>
-					<a href="https://www.linkedin.com/in/lucasjcfreire/" target="_blank" rel="noreferrer">
-						<FaLinkedin />
-					</a>
-				</li>
-				<li>
-					<a href="https://lucasjcfreire.vercel.app" target="_blank" rel="noreferrer">
-						<FaAddressCard />
-					</a>
-				</li>
-				<li>
-					<a href="https://github.com/LucasJCFreire" target="_blank" rel="noreferrer">
-						<FaGithub />
-					</a>
-				</li>
-			</ul>
-			<p>&copy; 2024 - Lucas Jones Dev</p>
-		</StyledFooter>
-	);
+  return (
+    <StyledFooter>
+      <ul>
+        <li>
+          <a href="https://www.linkedin.com/in/lucasjcfreire/" target="_blank" rel="noreferrer">
+            <FaLinkedin />
+          </a>
+        </li>
+        <li>
+          <a href="https://lucasjcfreire.vercel.app" target="_blank" rel="noreferrer">
+            <FaAddressCard />
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/LucasJCFreire" target="_blank" rel="noreferrer">
+            <FaGithub />
+          </a>
+        </li>
+      </ul>
+      <p>&copy; 2024 - Lucas Jones Dev</p>
+    </StyledFooter>
+  );
 }
 
 const StyledFooter = styled.footer`
@@ -31,7 +31,7 @@ const StyledFooter = styled.footer`
   bottom: 0;
   left:0;
   width: 100%;
-  height: 80px;
+  height: 100px;
   display: flex;
   flex-direction: column;
   justify-content: end;
